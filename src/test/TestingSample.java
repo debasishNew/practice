3f17e0db5c6fc8409bcd47af1874ba2731173ac8
bob <bob@bobamd40>
2018-04-16 15:39:19 +0530

    commit form jenkingn new

--- src/test/TestingSample.java
+++ src/test/TestingSample.java
@@ -14,6 +14,7 @@ public class TestingSample  {
 		
 		System.out.println("new commit to .git remote project2 debasishBranch1");
 		System.out.println("new commit to .git remote project2 debasishBranch2");
+		System.out.println("new commit to .git remote gitprojectjenkin");
 		int x=12;
 		System.out.println(x<<2);
 	}
