bc1c0b5ab6f05e1f71574226db469d141f435bbf
bob <bob@bobamd40>
2018-04-16 15:06:36 +0530

    first commit to repo

--- src/test/TestingSample.java
+++ src/test/TestingSample.java
@@ -18,6 +18,7 @@ public class TestingSample  {
 	public static void main(String[] args) throws Exception{
 		
 		System.out.println("new commit to .git remote");
+		System.out.println("new commit to .git remote second");
 		int x=12;
 		System.out.println(x<<2);
 	}
